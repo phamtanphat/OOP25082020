2e6bbed79781786d1db54d787c9c46b5f2d25f03
phatdroid94 <phatdroid94@gmail.com>
2020-09-24 20:24:56 +0700

    finish

---
+++ app/src/main/java/com/example/oop25082020/Person.java
@@ -0,0 +1,17 @@
+package com.example.oop25082020;
+
+public class Person {
+    private Animal animal;
+
+    public Person(Animal animal) {
+        this.animal = animal;
+    }
+
+    public Animal getAnimal() {
+        return animal;
+    }
+
+    public void setAnimal(Animal animal) {
+        this.animal = animal;
+    }
+}
