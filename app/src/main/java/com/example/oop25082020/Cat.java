df01ed944a1c51f5a8e91b131c416a099dc6d149
phatdroid94 <phatdroid94@gmail.com>
2020-09-24 20:18:02 +0700

    tu khoa extend va implement

--- app/src/main/java/com/example/oop25082020/Cat.java
+++ app/src/main/java/com/example/oop25082020/Cat.java
@@ -2,19 +2,25 @@ package com.example.oop25082020;
 
 import android.util.Log;
 
-public class Cat extends Animal implements OnRunAnimal,OnEatAnimal {
+public class Cat extends Animal implements OnPropertiesAnimal {
 
     public Cat(String name, Integer weight ) {
        super(name ,weight);
     }
 
-    @Override
-    public void run() {
-        Log.d("BBB","Chay bang 4 chan");
-    }
 
     @Override
     public void eat() {
+
+    }
+
+    @Override
+    public void listen() {
+
+    }
+
+    @Override
+    public void run() {
 
     }
 }
---
+++ app/src/main/java/com/example/oop25082020/OnPropertiesAnimal.java
@@ -0,0 +1,11 @@
+package com.example.oop25082020;
+
+public interface OnPropertiesAnimal extends OnEatAnimal,OnListenAnimal,OnRunAnimal {
+}
+
+// extend :
+//    - class voi class : chi extend 1 lan
+//    - interface voi interface : dung nhiu lan
+// implement :
+//    - class va interface : ke thua chuc nang va phai dinh nghia tinh nay trong class
+//    - abstract vs interface : no se truyen chuc nang va abstract va ai dung abstract class thi dinh nghia chuc nang nay
